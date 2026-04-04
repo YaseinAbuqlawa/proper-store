6e9ec84e6f7907f3247fa83bbeb58e8247484b6d
Yasein Abuqlawa <yaseinabuqlawa@gmail.com>
2026-04-04 21:10:20 +0200

    refactor(admin): consolidate image compression into ImageCompressor
    Remove ImageCompressionService in favor of the existing ImageCompressor
    static helper. Migrate AddCategoryUseCase and UpdateCollectionBannerUseCase
    to call ImageCompressor.compress() directly.

--- packages/admin_app/lib/features/store_config/domain/use_cases/add_category_use_case.dart
+++ packages/admin_app/lib/features/store_config/domain/use_cases/add_category_use_case.dart
@@ -1,24 +1,22 @@
 import 'dart:typed_data';
 
+import 'package:admin/core/failures/app_failures.dart';
+import 'package:admin/core/helpers/image_compressor.dart';
+import 'package:admin/features/store_config/domain/repo/store_config_repo.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:injectable/injectable.dart';
-
-import 'package:admin/core/failures/app_failures.dart';
-import 'package:admin/core/services/image_compression_service.dart';
-import 'package:admin/features/store_config/domain/repo/store_config_repo.dart';
 
 @lazySingleton
 class AddCategoryUseCase {
   final StoreConfigRepo _repo;
-  final ImageCompressionService _compressionService;
 
-  const AddCategoryUseCase(this._repo, this._compressionService);
+  const AddCategoryUseCase(this._repo);
 
   Future<Either<ServerFailure, void>> call({
     required String name,
     required Uint8List imageBytes,
   }) async {
-    final compressed = await _compressionService.compress(imageBytes);
+    final compressed = await ImageCompressor.compress(imageBytes);
     return _repo.addCategory(name: name, imageBytes: compressed);
   }
 }
